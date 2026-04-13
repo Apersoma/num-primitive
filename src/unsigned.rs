90a5cf5dc747e74fbb089d916a083c6130b16af4
Apersoma <95072312+Apersoma@users.noreply.github.com>
2026-04-13 17:15:52 -0400

    more cleanup

--- src/unsigned.rs
+++ src/unsigned.rs
@@ -34,15 +34,7 @@ use crate::{PrimitiveInteger, PrimitiveIntegerRef, PrimitiveSigned};
 /// assert_eq!(gcd::<u16>(1071, 462), 21);
 /// assert_eq!(gcd::<u32>(6_700_417, 2_147_483_647), 1);
 /// ```
-pub trait PrimitiveUnsigned: 
-    PrimitiveInteger 
-    + From<u8> 
-    + TryFrom<u8, Error = Infallible> 
-    + core::ops::Div<Self::NonZero> 
-    + core::ops::DivAssign<Self::NonZero>
-    + core::ops::Rem<Self::NonZero> 
-    + core::ops::RemAssign<Self::NonZero> 
-{
+pub trait PrimitiveUnsigned: PrimitiveInteger + From<u8> + TryFrom<u8, Error = Infallible> {
     /// The signed integer type used by methods like
     /// [`checked_add_signed`][Self::checked_add_signed].
     type Signed: PrimitiveSigned;
